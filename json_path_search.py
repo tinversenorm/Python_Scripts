7c71b161a397aab2aa08f53015f9d89af47c6c66
Pranav Harathi <phrocks254@gmail.com>
2016-08-10 12:36:00 -0500

    Updated error checking, added single path checking

--- json_path_search.py
+++ json_path_search.py
@@ -3,17 +3,15 @@ import json
 # and returns whether an element exists at that path
 # Author: Pranav Harathi
 # Date: 06-22-16
- 
-"""
-path = raw_input("Enter the path you want to search: ");
-#path = "Organization.Linkage.ShareOwner.ShareHoldingPercentage"
-path = path.strip();
-path_points = path.split(".");
-"""
- 
-filename_paths = raw_input("Enter the name of the file containing the paths on separate lines (and nothing else): ")
-paths = [line.strip() for line in open(filename_paths)]
- 
+
+path_search_type = raw_input("Search by path or with multiple paths? (path/paths): ");
+if(path_search_type == "path"): 
+        path = raw_input("Enter the path you want to search: ");
+        paths = [path]
+elif(path_search_type == "pathset"):
+        filename_paths = raw_input("Enter the name of the file containing the paths on separate lines (and nothing else): ")
+        paths = [line.strip() for line in open(filename_paths)]
+
 # reads in json to search from a file
 filename = raw_input("Enter the name of the file containing the JSON: ");
 jsonfile = open(filename, "r");
@@ -28,14 +26,19 @@ path_confirm = True
 while path_confirm:
                 starting_path_q = raw_input("Is your starting path \"OrderProductResponse.OrderProductResponseDetail.Product\"? (y/n)  ")
                 if(starting_path_q == "y" or starting_path_q == "Y"):
-                                print "Analyzing data..."
-                                print ""
-                                break
+                        starting_path = "OrderProductResponse.OrderProductResponseDetail.Product"
+                        print "Analyzing data..."
+                        print ""
+                        break
                 elif(starting_path_q == "n" or starting_path_q == "N"):
                                 while path_confirm:
-                                                starting_path = raw_input("Enter your starting path: (Level.sublevel.subsublevel...)  ")
+                                                starting_path = raw_input("Enter your starting path: (Level.sublevel.subsublevel... or nothing if you start from the top level)  ")
+                                                if(starting_path == "" or starting_path == "nothing"):
+                                                        starting_path_confirm = "Top-level starting path"
+                                                else:
+                                                        starting_path_confirm = starting_path
                                                 while True:
-                                                                confirmation = raw_input("Is this correct? " + starting_path + " (y/n)  ")
+                                                                confirmation = raw_input("Is this correct? \"" + starting_path_confirm + "\" (y/n)  ")
                                                                 if(confirmation == "y" or confirmation == "Y"):
                                                                                 print "Analyzing data..."
                                                                                 print ""
@@ -48,23 +51,20 @@ while path_confirm:
                 else:
                                 print "Invalid input"
  
-if(starting_path == ""):
-                starting_path = "OrderProductResponse.OrderProductResponseDetail.Product"
- 
-def check_path_exists(path):
+
+def check_path_exists(path, starting_path):
                 path = path.strip();
                 path_points = path.split(".");
  
                 # starting path to json path
                 current = json
-                for startpathpoint in starting_path.split("."):
+                if(starting_path != ""):
+                        for startpathpoint in starting_path.split("."):
                                 current = current[startpathpoint]
  
                 # checks if path exists
                 success = True
-                #current = json["OrderProductResponse"]["OrderProductResponseDetail"]["Product"]
                 for point in path_points:
-                                #print point
                                 if type(current) is list:        # if it is a list, iterate until the next json object is found
                                                 #print current
                                                 check = None    # reference for next current
@@ -96,5 +96,5 @@ def check_path_exists(path):
  
 found = 0
 for path in paths:
-                if(check_path_exists(path)):
+                if(check_path_exists(path, starting_path)):
                                 found += 1
